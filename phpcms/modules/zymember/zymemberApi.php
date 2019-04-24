94b3737d31eaf0496de93f7ad6847aea2542b77f
glu <605468025@qq.com>
2019-04-24 17:33:26 +0800

    12

--- phpcms/modules/zymember/zymemberApi.php
+++ phpcms/modules/zymember/zymemberApi.php
@@ -2,7 +2,7 @@
 defined('IN_PHPCMS') or exit('No permission resources.');
 pc_base::load_app_func('global');
 
-class api{
+class zymemberApi{
 	function __construct() {
 
 		$this->get_db = pc_base::load_model('get_model');
@@ -18,8 +18,6 @@ class api{
 		$this->member_collect_db = pc_base::load_model('member_collect_model');
 		$this->member_footprint_db = pc_base::load_model('member_footprint_model');
 	}
-
-
 
 	/**
 	* 会员注册协议
@@ -43,81 +41,188 @@ class api{
 		//==================	操作成功-更新数据 END
 	}
 
-
-
 	/**
-	* 注销用户
-	* @status [状态] -1用户id不能为空/-2帐号不存在/-3帐号已锁定
-	* @param  [type] $userid [*用户ID]
-	* @param  [type] $type [*类型：1web端、2APP端]
-	* @param  [type] $forward [接下来该跳转的页面链接]
-	* @return [json] [json数组]
-	*/
-	public function logout()
+	 * 注册_手机短信注册
+	 * @status [状态] -1帐号、密码、验证码不能为空/-2用户名格式错误/-3密码格式错误/-4验证码错误/-5帐号已存在
+	 * @param  [type] $mobile [*用户帐号]
+	 * @param  [type] $verify_code [*手机验证码]
+	 * @param  [type] $password [*用户密码]
+	 * @param  [type] $type [*类型：1web端、2APP端]
+	 * @param  [type] $forward [接下来该跳转的页面链接]
+	 * @return [json] [json数组]
+	 */
+	public function sms_registered()
 	{
-		$userid = $_POST['userid'];	//帐号（暂定为手机号）
-		$type = $_POST['type'] ? $_POST['type'] : 1;	//类型：1web端、2APP端
-		$forward = $_POST['forward'] ? urldecode($_POST['forward']) : APP_PATH.'index.php?m=member&c=index&a=login';	//接下来该跳转的页面链接
-		
+		$mobile = $_POST['mobile'];	//手机号
+		$verify_code = $_POST['verify_code'];	//短信验证码
+		$password = $_POST['password'];	//密码
+		$type = $_POST['type'];	//类型：1web端、2APP端
+		$forward = $_POST['forward'] ? urldecode($_POST['forward']) : APP_PATH.'index.php?m=member&c=index';	//接下来该跳转的页面链接
+
 		//用手机号码查出用户账号
-		$memberinfo = $this->member_db->get_one(array('userid'=>$userid));
+		$memberinfo = $this->member_db->get_one(array('mobile'=>$mobile));
+
 		//==================	操作失败-验证 START
-			//帐号密码类型不能为空
-			if (!$userid) {
-				$result = [
-					'status'=>'error',
-					'code'=>-1,
-					'message'=>'用户id不能为空',
-					
-				];
-				exit(json_encode($result,JSON_UNESCAPED_UNICODE|JSON_PRETTY_PRINT));
-			}
-			//账号不存在
-			if (!$memberinfo) {
-				$result = [
-					'status'=>'error',
-					'code'=>-2,
-					'message'=>'账号不存在',	//账号不存在
-					
-				];
-				exit(json_encode($result,JSON_UNESCAPED_UNICODE|JSON_PRETTY_PRINT));
-			}
-			//帐号已锁定,无法登录
-			if($memberinfo['islock']==1) {
-				$result = [
-					'status'=>'error',
-					'code'=>-3,
-					'message'=>'帐号已锁定,无法登录',	//帐号已锁定,无法登录
-					
-				];
-				exit(json_encode($result,JSON_UNESCAPED_UNICODE|JSON_PRETTY_PRINT));
-			}
-		//==================	操作失败-验证 END
-
-		//==================	操作成功-更新数据 START
-
-			//如果是网页的话，清空缓存。如果是APP的话，还没定
-			if ($type==1) {
-				param::set_cookie('auth', '');
-				param::set_cookie('_userid', '');
-				param::set_cookie('_username', '');
-				param::set_cookie('_groupid', '');
-				param::set_cookie('_nickname', '');
-				param::set_cookie('cookietime', '');
-			}
-
+		//帐号密码类型不能为空
+		if (!$mobile || !$verify_code || !$password) {
 			$result = [
-				'status'=>'success',
-				'code'=>200,
-				'message'=>'注销成功',
-				'data'=>[
-					'forward'=>$forward	//给web端用的，接下来跳转到哪里
-				]
+				'status'=>'error',
+				'code'=>-1,
+				'message'=>'帐号、验证码、密码不能为空',
+
 			];
 			exit(json_encode($result,JSON_UNESCAPED_UNICODE|JSON_PRETTY_PRINT));
-		//==================	操作成功-更新数据 END
-	}
+		}
+		//用户名格式验证（手机号码格式验证）
+		if (!$this->_verify_ismobile($mobile)) {
+			$result = [
+				'status'=>'error',
+				'code'=>-2,
+				'message'=>'用户名格式错误',	//只允许 13，14，15，16，17，18，19的号码,11位
 
+			];
+			exit(json_encode($result,JSON_UNESCAPED_UNICODE|JSON_PRETTY_PRINT));
+		}
+		//密码格式错误
+		if (!$this->_verify_ispassword($password)) {
+			$result = [
+				'status'=>'error',
+				'code'=>-3,
+				'message'=>'密码格式错误',	//6-16位之间,只允许数字、大小写英文、下划线
+
+			];
+			exit(json_encode($result,JSON_UNESCAPED_UNICODE|JSON_PRETTY_PRINT));
+		}
+		//短信验证码错误
+		//调用通讯模块-短信接口-查询此账号的短信验证码是否匹配上了
+		//$sms_verify = true;
+		//==================	获取其他接口-接口 START
+		$config = $this->zyconfig_db->get_one(array('key'=>'zymessagesys4'),"url");
+		$curl = [
+			'mobile'=>$mobile,
+			'verify_code'=>$verify_code,
+			'clear'=>1,
+		];
+		$sms_verify = _crul_post($config['url'],$curl);
+		$sms_verify=json_decode($sms_verify,true);
+		//==================	获取其他接口-接口 END
+
+
+		if($sms_verify['status']=='error') {	//false,进入
+			$result = [
+				'status'=>'error',
+				'code'=>-4,
+				'message'=>$sms_verify['message'],	//短信验证码错误
+
+			];
+			exit(json_encode($result,JSON_UNESCAPED_UNICODE|JSON_PRETTY_PRINT));
+		}
+		//帐号已存在
+		if ($memberinfo) {
+			$result = [
+				'status'=>'error',
+				'code'=>-5,
+				'message'=>'帐号已存在',	//帐号已存在
+
+			];
+			exit(json_encode($result,JSON_UNESCAPED_UNICODE|JSON_PRETTY_PRINT));
+		}
+		//==================	操作失败-验证 END
+
+		//==================	操作成功-插入数据 START
+
+		//获取会员基本设置的配置
+		$member_setting = $this->module_db->get_one(array('module'=>'member'), 'setting');
+		$member_setting = string2array($member_setting['setting']);
+
+		$userinfo = array();
+		//用户基本信息
+		$userinfo['username'] = create_randomstr(8);
+		$userinfo['password'] = $password;
+		$userinfo['encrypt'] = create_randomstr(6);
+		$userinfo['nickname'] = $mobile;
+		$userinfo['regdate'] = time();
+		$userinfo['regip'] = ip();
+		$userinfo['email'] = time().'@300c.cn';
+		$userinfo['groupid'] = 2;
+		$userinfo['amount'] = 0;
+		$userinfo['point'] = $member_setting['defualtpoint'];
+		$userinfo['modelid'] = 10;
+		//$userinfo['islock'] = $_POST['info']['islock']==1 ? 0 : 1;
+		//$userinfo['vip'] = $_POST['info']['vip']==1 ? 1 : 0;
+		//$userinfo['overduedate'] = strtotime($_POST['info']['overduedate']);
+		$userinfo['mobile'] = $mobile;
+
+
+		//传入phpsso为明文密码，加密后存入phpcms_v9
+		$password = $userinfo['password'];
+		$userinfo['password'] = password($userinfo['password'], $userinfo['encrypt']);
+
+
+		//主表
+		$userid=$this->member_db->insert($userinfo,true);
+		$this->member_db->update(array('phpssouid'=>$userid),'userid='.$userid);
+
+		//sso表
+		$sso_members_db = pc_base::load_model('sso_members_model');
+		$data_member_sso = array(
+			'username'=>$userinfo['username'],
+			'password'=>$userinfo['password'],
+			'random'=>$userinfo['encrypt'],
+			'email'=>$userinfo['email'],
+			'regdate'=>$userinfo['regdate'],
+			'lastdate'=>$userinfo['regdate'],
+			'regip'=>$userinfo['regip'],
+			'lastip'=>$userinfo['lastip'],
+			'appname'=>'phpcmsv9',
+			'type'=>'app',
+		);
+		$sso_members_db->insert($data_member_sso);
+
+		//附表
+		$data_member_detail = array(
+			'userid'=>$userid,
+		);
+		$this->member_detail_db->insert($data_member_detail);
+
+		$memberinfo = $this->member_db->get_one(array('mobile'=>$mobile));
+		//如果是网页的话，要存缓存。如果是APP的话，我就直接传值就行了
+		if ($type==1) {
+			$cookietime = SYS_TIME + 7200;	//系统时间+两个小时
+			$phpcms_auth = sys_auth($memberinfo['userid']."\t".$memberinfo['password'], 'ENCODE', get_auth_key('login'));
+			param::set_cookie('auth', $phpcms_auth, $cookietime);
+			param::set_cookie('_userid', $memberinfo['userid'], $cookietime);
+			param::set_cookie('_username', $memberinfo['username'], $cookietime);
+			param::set_cookie('_nickname', $memberinfo['nickname'], $cookietime);
+			param::set_cookie('_groupid', $memberinfo['groupid'], $cookietime);
+			param::set_cookie('cookietime', $_cookietime, $cookietime);
+		}
+
+		//调用通讯模块-短信接口-清空此账号的短信验证码
+		//操作成功之后删除遗留的短信验证码
+		//==================	获取其他接口-接口 START
+		$config = $this->zyconfig_db->get_one(array('key'=>'zymessagesys5'),"url");
+		$curl = [
+			'mobile'=>$memberinfo['mobile']
+		];
+		_crul_post($config['url'],$curl);
+		//==================	获取其他接口-接口 END
+
+
+		$result = [
+			'status'=>'success',
+			'code'=>200,
+			'message'=>'注册成功',
+			'data'=>[
+				'userid'=>$memberinfo['userid'],
+				'groupid'=>$memberinfo['groupid'],
+				'forward'=>$forward,	//给web端用的，接下来跳转到哪里
+			]
+		];
+		exit(json_encode($result,JSON_UNESCAPED_UNICODE|JSON_PRETTY_PRINT));
+		//==================	操作成功-插入数据 END
+
+	}
 
 	/**
 	* 登录_帐号密码登录
@@ -365,190 +470,6 @@ class api{
 			exit(json_encode($result,JSON_UNESCAPED_UNICODE|JSON_PRETTY_PRINT));
 		//==================	操作成功-返回数据 END
 
-	}
-
-
-	/**
-	* 注册_手机短信注册
-	* @status [状态] -1帐号、密码、验证码不能为空/-2用户名格式错误/-3密码格式错误/-4验证码错误/-5帐号已存在
-	* @param  [type] $mobile [*用户帐号]
-	* @param  [type] $verify_code [*手机验证码]
-	* @param  [type] $password [*用户密码]
-	* @param  [type] $type [*类型：1web端、2APP端]
-	* @param  [type] $forward [接下来该跳转的页面链接]
-	* @return [json] [json数组]
-	*/
-	public function sms_registered()
-	{
-		$mobile = $_POST['mobile'];	//手机号
-		$verify_code = $_POST['verify_code'];	//短信验证码
-		$password = $_POST['password'];	//密码
-		$type = $_POST['type'];	//类型：1web端、2APP端
-		$forward = $_POST['forward'] ? urldecode($_POST['forward']) : APP_PATH.'index.php?m=member&c=index';	//接下来该跳转的页面链接
-
-		//用手机号码查出用户账号
-		$memberinfo = $this->member_db->get_one(array('mobile'=>$mobile));
-
-		//==================	操作失败-验证 START
-			//帐号密码类型不能为空
-			if (!$mobile || !$verify_code || !$password) {
-				$result = [
-					'status'=>'error',
-					'code'=>-1,
-					'message'=>'帐号、验证码、密码不能为空',
-					
-				];
-				exit(json_encode($result,JSON_UNESCAPED_UNICODE|JSON_PRETTY_PRINT));
-			}
-			//用户名格式验证（手机号码格式验证）
-			if (!$this->_verify_ismobile($mobile)) {
-				$result = [
-					'status'=>'error',
-					'code'=>-2,
-					'message'=>'用户名格式错误',	//只允许 13，14，15，16，17，18，19的号码,11位
-					
-				];
-				exit(json_encode($result,JSON_UNESCAPED_UNICODE|JSON_PRETTY_PRINT));
-			}
-			//密码格式错误
-			if (!$this->_verify_ispassword($password)) {
-				$result = [
-					'status'=>'error',
-					'code'=>-3,
-					'message'=>'密码格式错误',	//6-16位之间,只允许数字、大小写英文、下划线
-					
-				];
-				exit(json_encode($result,JSON_UNESCAPED_UNICODE|JSON_PRETTY_PRINT));
-			}
-			//短信验证码错误
-			//调用通讯模块-短信接口-查询此账号的短信验证码是否匹配上了
-			//$sms_verify = true;
-			//==================	获取其他接口-接口 START
-				$config = $this->zyconfig_db->get_one(array('key'=>'zymessagesys4'),"url");
-				$curl = [
-					'mobile'=>$mobile,
-					'verify_code'=>$verify_code,
-					'clear'=>1,
-				];
-				$sms_verify = _crul_post($config['url'],$curl);
-				$sms_verify=json_decode($sms_verify,true);
-			//==================	获取其他接口-接口 END		
-
-
-			if($sms_verify['status']=='error') {	//false,进入
-				$result = [
-					'status'=>'error',
-					'code'=>-4,
-					'message'=>$sms_verify['message'],	//短信验证码错误
-					
-				];
-				exit(json_encode($result,JSON_UNESCAPED_UNICODE|JSON_PRETTY_PRINT));
-			}
-			//帐号已存在
-			if ($memberinfo) {
-				$result = [
-					'status'=>'error',
-					'code'=>-5,
-					'message'=>'帐号已存在',	//帐号已存在
-					
-				];
-				exit(json_encode($result,JSON_UNESCAPED_UNICODE|JSON_PRETTY_PRINT));
-			}
-		//==================	操作失败-验证 END		
-
-		//==================	操作成功-插入数据 START
-
-			//获取会员基本设置的配置
-			$member_setting = $this->module_db->get_one(array('module'=>'member'), 'setting');
-			$member_setting = string2array($member_setting['setting']);
-
-			$userinfo = array();
-			//用户基本信息
-			$userinfo['username'] = create_randomstr(8);
-			$userinfo['password'] = $password;
-			$userinfo['encrypt'] = create_randomstr(6);
-			$userinfo['nickname'] = $mobile;
-			$userinfo['regdate'] = time();
-			$userinfo['regip'] = ip();
-			$userinfo['email'] = time().'@300c.cn';
-			$userinfo['groupid'] = 2;
-			$userinfo['amount'] = 0;
-			$userinfo['point'] = $member_setting['defualtpoint'];
-			$userinfo['modelid'] = 10;
-			//$userinfo['islock'] = $_POST['info']['islock']==1 ? 0 : 1;
-			//$userinfo['vip'] = $_POST['info']['vip']==1 ? 1 : 0;
-			//$userinfo['overduedate'] = strtotime($_POST['info']['overduedate']);
-			$userinfo['mobile'] = $mobile;
-
-			
-			//传入phpsso为明文密码，加密后存入phpcms_v9
-			$password = $userinfo['password'];
-			$userinfo['password'] = password($userinfo['password'], $userinfo['encrypt']);
-
-
-			//主表
-			$userid=$this->member_db->insert($userinfo,true);
-			$this->member_db->update(array('phpssouid'=>$userid),'userid='.$userid);
-			
-			//sso表
-			$sso_members_db = pc_base::load_model('sso_members_model');
-			$data_member_sso = array(
-				'username'=>$userinfo['username'],
-				'password'=>$userinfo['password'],
-				'random'=>$userinfo['encrypt'],
-				'email'=>$userinfo['email'],
-				'regdate'=>$userinfo['regdate'],
-				'lastdate'=>$userinfo['regdate'],
-				'regip'=>$userinfo['regip'],
-				'lastip'=>$userinfo['lastip'],
-				'appname'=>'phpcmsv9',
-				'type'=>'app',
-			);	
-			$sso_members_db->insert($data_member_sso);
-			
-			//附表
-			$data_member_detail = array(
-				'userid'=>$userid,
-			);	
-			$this->member_detail_db->insert($data_member_detail);
-
-			$memberinfo = $this->member_db->get_one(array('mobile'=>$mobile));
-			//如果是网页的话，要存缓存。如果是APP的话，我就直接传值就行了
-			if ($type==1) {
-				$cookietime = SYS_TIME + 7200;	//系统时间+两个小时
-				$phpcms_auth = sys_auth($memberinfo['userid']."\t".$memberinfo['password'], 'ENCODE', get_auth_key('login'));
-				param::set_cookie('auth', $phpcms_auth, $cookietime);
-				param::set_cookie('_userid', $memberinfo['userid'], $cookietime);
-				param::set_cookie('_username', $memberinfo['username'], $cookietime);
-				param::set_cookie('_nickname', $memberinfo['nickname'], $cookietime);
-				param::set_cookie('_groupid', $memberinfo['groupid'], $cookietime);
-				param::set_cookie('cookietime', $_cookietime, $cookietime);
-			}
-			
-			//调用通讯模块-短信接口-清空此账号的短信验证码
-			//操作成功之后删除遗留的短信验证码
-			//==================	获取其他接口-接口 START
-				$config = $this->zyconfig_db->get_one(array('key'=>'zymessagesys5'),"url");
-				$curl = [
-					'mobile'=>$memberinfo['mobile']
-				];
-				_crul_post($config['url'],$curl);
-			//==================	获取其他接口-接口 END		
-
-
-			$result = [
-				'status'=>'success',
-				'code'=>200,
-				'message'=>'注册成功',
-				'data'=>[
-					'userid'=>$memberinfo['userid'],
-					'groupid'=>$memberinfo['groupid'],
-					'forward'=>$forward,	//给web端用的，接下来跳转到哪里
-				]
-			];
-			exit(json_encode($result,JSON_UNESCAPED_UNICODE|JSON_PRETTY_PRINT));
-		//==================	操作成功-插入数据 END
-		
 	}
 
 	/**
